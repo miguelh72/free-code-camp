7f512a7d4487f8698c8306145651ac7d5707724b
Miguel Hernandez <miguelh72@outlook.com>
2020-05-25 13:08:45 -0400

    Added file for first project

---
+++ JavaScript Algorithms and Data Structures Certification/palindrome_checker.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Remove all symbols and spaces
+function validate(str) {
+  return;
+}
+
+// Problem statement: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker
+function palindrome(str) {
+  return;
+}
