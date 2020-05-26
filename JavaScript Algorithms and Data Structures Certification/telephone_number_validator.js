4fd9840a3c9da0ea2236d2a0753a606d99d3dc32
Miguel Hernandez <miguelh72@outlook.com>
2020-05-26 11:59:51 -0400

    Finished first draft of Telephone Number Validator problem on mobile. Need to test on freeCodeCamp once on pc.

--- JavaScript Algorithms and Data Structures Certification/telephone_number_validator.js
+++ JavaScript Algorithms and Data Structures Certification/telephone_number_validator.js
@@ -1,7 +1,9 @@
 // Problem statement:
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/telephone-number-validator
 function telephoneCheck(str) {
-    return true;
+    if (!(str) || str.length < 10) return false;
+    
+    return /^(1(\s|-)?)?(\(\d{3}\)|\d{3})(\s|-)?\d{3}(\s|-)?\d{4}$/.test(str);
 }
 
 module.exports = { telephoneCheck };
--- JavaScript Algorithms and Data Structures Certification/telephone_number_validator.test.js
+++ JavaScript Algorithms and Data Structures Certification/telephone_number_validator.test.js
@@ -4,10 +4,32 @@ test("Implemented problem interface", () => {
     expect(telephoneCheck).not.toBeFalsy();
 });
 
-// Check boolean truthy for valid phone numbers
+test("Return false for no input", () => {
+    expect(telephoneCheck()).toBe(false);
+});
 
-// Check truthy for numbers with valid US country code
+test("Return boolean true for valid phone number formats", () => {
+    expect(telephoneCheck("5555555555")).toBe(true);
+    expect(telephoneCheck("555 555 5555")).toBe(true);
+    expect(telephoneCheck("555-555-5555")).toBe(true);
+    expect(telephoneCheck("(555) 555-5555")).toBe(true);
+});
 
-// Check falsy for numbers with invalid country code
+test("Return boolean true for valid phone numbers with a US area code.", () => {
+    expect(telephoneCheck("1 (555) 555-5555")).toBe(true);
+    expect(telephoneCheck("15555555555")).toBe(true);
+    expect(telephoneCheck("1-555-555-5555")).toBe(true);
+});
 
-// Check boolean falsy for invalid phone numbers
+test("Return boolean false for valid phone numbers with a non US area code.", () => {
+    expect(telephoneCheck("2 (555) 555-5555")).toBe(false);
+    expect(telephoneCheck("+645555555555")).toBe(false);
+    expect(telephoneCheck("11-555-555-5555")).toBe(false);
+});
+
+test("Return boolean false for invalid phone numbers.", () => {
+    expect(telephoneCheck("555-5555")).toBe(false);
+    expect(telephoneCheck("1 555)555-5555")).toBe(false);
+    expect(telephoneCheck("(6054756961)")).toBe(false);
+    expect(telephoneCheck("(275)76227382)")).toBe(false);
+});
