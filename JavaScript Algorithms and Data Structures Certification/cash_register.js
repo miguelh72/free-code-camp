c7325618386e730507be904073da620e5c8f0661
Miguel Hernandez <miguelh72@outlook.com>
2020-05-26 23:01:00 -0400

    Finished first draft of Cash Register problem. Solution passes freeCodeCamp test and was accepted. Consider changing to OO or functional paradigm instead of procedural.

--- JavaScript Algorithms and Data Structures Certification/cash_register.js
+++ JavaScript Algorithms and Data Structures Certification/cash_register.js
@@ -3,21 +3,52 @@
 // Problem statement:
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/cash-register
 function checkCashRegister(price, cash, cid) {
-    const valueInPennies = {
+    // NOTE: All money inside function will be in units of pennies, and will only be converted to ones at end.
+    const VALUE = {
         PENNY: 1,
         NICKEL: 5,
         DIME: 10,
         QUARTER: 25,
         ONE: 100,
         FIVE: 500,
-        TEN: 10000,
-        TWENTY: 20000,
-        "ONE HUNDRED": 100000,
+        TEN: 1000,
+        TWENTY: 2000,
+        "ONE HUNDRED": 10000,
     };
-    
-    const changeInPenies = Math.round(price*1000 - cash*1000);
-    
-    return { status: "OPEN", change: true };
+    price = Math.round(price * VALUE["ONE"]);
+    cash = Math.round(cash * VALUE["ONE"]);
+    const cashRegister = cid.reduce((drawer, [label, valueInOnes]) => {
+        const valueInPennies = Math.round(valueInOnes * VALUE["ONE"]);
+        drawer[label] = Math.round(valueInPennies / VALUE[label]);
+        drawer.total += valueInPennies;
+        return drawer;
+    }, { total:0 });
+
+    const change = cash - price;
+    if (cashRegister.total < change) return {status: "INSUFFICIENT_FUNDS", change: []};
+    if (cashRegister.total === change) return {status: "CLOSED", change: cid};
+
+    let remainingChange = change;
+    let foundChange = 0;
+    const calcChange =  ["ONE HUNDRED", "TWENTY", "TEN", "FIVE", "ONE", "QUARTER", "DIME", "NICKEL", "PENNY"]
+        .reduce((out, label) => {
+            let numUnits = 0;
+            while(remainingChange >= VALUE[label] && cashRegister[label] > 0) {
+                cashRegister[label]--;
+                cashRegister.total -= VALUE[label];
+                remainingChange -= VALUE[label];
+                numUnits++;
+            }
+            if (numUnits) {
+                out.push([label, (numUnits*VALUE[label]) / VALUE["ONE"]]);
+                foundChange += numUnits*VALUE[label];
+            }
+            return out;
+        }, []);
+    if (foundChange === change)
+        return {status: "OPEN", change: calcChange};
+    else
+        return {status: "INSUFFICIENT_FUNDS", change: []};
 }
 
 module.exports = { checkCashRegister };
--- JavaScript Algorithms and Data Structures Certification/cash_register.test.js
+++ JavaScript Algorithms and Data Structures Certification/cash_register.test.js
@@ -60,4 +60,102 @@ test("Returns correct change when change is less than available in change drawer
         status: "OPEN", 
         change: [["QUARTER", 0.5]]
     });
+
+    expect(checkCashRegister(
+        3.26, 
+        100, 
+        [
+            ["PENNY", 1.01], 
+            ["NICKEL", 2.05], 
+            ["DIME", 3.1], 
+            ["QUARTER", 4.25], 
+            ["ONE", 90], 
+            ["FIVE", 55], 
+            ["TEN", 20], 
+            ["TWENTY", 60], 
+            ["ONE HUNDRED", 100]
+        ])
+    ).toEqual({
+        status: "OPEN", 
+        change: [
+            ["TWENTY", 60], 
+            ["TEN", 20], 
+            ["FIVE", 15], 
+            ["ONE", 1], 
+            ["QUARTER", 0.5], 
+            ["DIME", 0.2], 
+            ["PENNY", 0.04]
+        ]
+    });
+});
+
+test("Return insufficient fund object when there isnt enough chainge in cash register.", () => {
+    expect(checkCashRegister(
+        19.5, 
+        20, 
+        [
+            ["PENNY", 0.01], 
+            ["NICKEL", 0], 
+            ["DIME", 0], 
+            ["QUARTER", 0], 
+            ["ONE", 0], 
+            ["FIVE", 0], 
+            ["TEN", 0], 
+            ["TWENTY", 0], 
+            ["ONE HUNDRED", 0]
+        ])
+    ).toEqual({
+        status: "INSUFFICIENT_FUNDS", 
+        change: []
+    });
+
+    expect(checkCashRegister(
+        19.5, 
+        20, 
+        [
+            ["PENNY", 0.01], 
+            ["NICKEL", 0], 
+            ["DIME", 0], 
+            ["QUARTER", 0], 
+            ["ONE", 1], 
+            ["FIVE", 0], 
+            ["TEN", 0], 
+            ["TWENTY", 0], 
+            ["ONE HUNDRED", 0]
+        ])
+    ).toEqual({
+        status: "INSUFFICIENT_FUNDS", 
+        change: []
+    });
+});
+
+test("Return change with CLOSED status when change is exact amount in cash register.", () => {
+    expect(checkCashRegister(
+        19.5, 
+        20, 
+        [
+            ["PENNY", 0.5], 
+            ["NICKEL", 0], 
+            ["DIME", 0], 
+            ["QUARTER", 0], 
+            ["ONE", 0], 
+            ["FIVE", 0], 
+            ["TEN", 0], 
+            ["TWENTY", 0], 
+            ["ONE HUNDRED", 0]
+        ])
+    ).toEqual({
+        status: "CLOSED", 
+        change: [
+            ["PENNY", 0.5], 
+            ["NICKEL", 0], 
+            ["DIME", 0], 
+            ["QUARTER", 0], 
+            ["ONE", 0], 
+            ["FIVE", 0], 
+            ["TEN", 0], 
+            ["TWENTY", 0], 
+            ["ONE HUNDRED", 0]
+        ]
+    });
 });
